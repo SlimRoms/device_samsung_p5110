15dc4d2fb356c754027ce165b834920a53972ec3
Andreas Blaesius <skate4life@gmx.de>
2015-08-03 21:51:19 +0200

    P5110: ro.carrier needs to be defined earlier
    ... else it get overwriten by generic_no_telephony.mk which
    get inherit from full_base.mk
    
    ( Line 58+59 https://android.googlesource.com/platform/build/+/android-5.1.1_r8/target/product/generic_no_telephony.mk )
    
    Change-Id: Id974e3509b8fb09a6af17e9807b08289bf80c982

--- slim.mk
+++ slim.mk
@@ -23,6 +23,9 @@ PRODUCT_RELEASE_NAME := p5110
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 1280
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carrier=wifi-only
+
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
 
